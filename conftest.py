0d3c5e87a46c45ea69b7a116895f50c42a5b3540
Maxim Gonchar <m.gonchar@bi.zone>
2021-03-23 00:23:39 +0300

    homework - choose language for Chrome refactoring add choose browser

--- conftest.py
+++ conftest.py
@@ -2,17 +2,30 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="Chrome",
+                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
                      help="Select language: ar, ru, en, ... ")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     site_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': site_language})
-    print("\nStart browser Chrome fot test...")
-    browser = webdriver.Chrome(options=options)
+    if browser_name == "Chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': site_language})
+        print("\nStart browser Chrome fot test...")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", site_language)
+        print("\nStart browser Firefox fot test...")
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        print(f"\nUnknow browser " + browser_name + " is choose. Please, select Chrome or Firefox ")
     yield browser
     print("\nQuit browser..")
     browser.quit()
+
