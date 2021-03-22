c673e6450555ef68d6c32903b3ea941366852fea
Maxim Gonchar <m.gonchar@bi.zone>
2021-03-22 23:53:03 +0300

    homework - choose language for Chrome refactoring

--- test_language/test_items.py
+++ test_language/test_items.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 class TestLang:
-    def test_choose_language(self, browser):
+    def test_choose_page_language(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         wait = WebDriverWait(browser, 5)
         browser.get(link)
